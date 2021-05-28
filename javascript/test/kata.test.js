493addf393082c90a52eb7b60a0615dd2567eb1b
Luis Rovirosa Mairlot <luis@codium.team>
2021-05-28 18:37:32 +0200

    improve test names

--- javascript/test/kata.test.js
+++ javascript/test/kata.test.js
@@ -1,7 +1,7 @@
 const { Game } = require("../src/kata");
 
 describe("Test for Game", function () {
-  it("If you strike one bawl the roll score is one", function () {
+  it("If you knock one pin the roll score is one", function () {
     const game = new Game();
 
     game.roll(1);
@@ -9,7 +9,7 @@ describe("Test for Game", function () {
     expect(game.score()).toBe(1);
   });
 
-  it("If you strike two bawl the roll score is two", function () {
+  it("If you knock two pins the roll score is two", function () {
     const game = new Game();
 
     game.roll(2);
