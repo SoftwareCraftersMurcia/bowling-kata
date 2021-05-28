a12c08731327769dfc28c40ce427213bc9d9c591
Luis Rovirosa Mairlot <luis@codium.team>
2021-05-28 18:38:17 +0200

    add setup

--- javascript/test/kata.test.js
+++ javascript/test/kata.test.js
@@ -1,17 +1,17 @@
-const { Game } = require("../src/kata");
+const {Game} = require('../src/kata');
 
-describe("Test for Game", function () {
-  it("If you knock one pin the roll score is one", function () {
-    const game = new Game();
-
+describe('Test for Game', function() {
+  let game;
+  beforeEach(() => {
+    game = new Game();
+  });
+  it('If you knock one pin the roll score is one', function() {
     game.roll(1);
 
     expect(game.score()).toBe(1);
   });
 
-  it("If you knock two pins the roll score is two", function () {
-    const game = new Game();
-
+  it('If you knock two pins the roll score is two', function() {
     game.roll(2);
 
     expect(game.score()).toBe(2);
