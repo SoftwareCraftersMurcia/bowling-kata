ba721a37f9e2d5908c355a053c2e8fbfded89ced
Pascual <pascual@strata3.com>
2021-05-28 19:14:00 +0200

    Create previousRollWasStrike state

--- javascript/src/kata.js
+++ javascript/src/kata.js
@@ -1,14 +1,16 @@
 class Game {
   constructor() {
     this.currentScore = 0;
+    this.previousRollWasStrike = false;
   }
 
   roll(n) {
-    if(this.currentScore === 10) {
-      this.currentScore += 2*n;
+    if (this.previousRollWasStrike) {
+      this.currentScore += 2 * n;
     } else {
       this.currentScore += n;
     }
+    if (n === 10) this.previousRollWasStrike = true;
   }
 
   score() {
--- javascript/test/kata.test.js
+++ javascript/test/kata.test.js
@@ -1,44 +1,50 @@
-const {Game} = require('../src/kata');
+const { Game } = require("../src/kata");
 
-describe('Test for Game', () => {
+describe("Test for Game", () => {
   let game;
 
   beforeEach(() => {
     game = new Game();
   });
 
-  describe('with one single roll',  () =>{
-    it('If you knock one pin the roll score is one', () => {
+  describe("with one single roll", () => {
+    it("If you knock one pin the roll score is one", () => {
       game.roll(1);
 
       expect(game.score()).toBe(1);
     });
 
-    it('If you knock two pins the roll score is two', () => {
+    it("If you knock two pins the roll score is two", () => {
       game.roll(2);
 
       expect(game.score()).toBe(2);
     });
   });
 
-  describe('with several rolls',  () => {
-    it('should sum up both rolls',  () => {
+  describe("with several rolls", () => {
+    it("should sum up both rolls", () => {
       game.roll(1);
       game.roll(1);
+      expect(game.score()).toBe(2);
+    });
 
-      expect(game.score()).toBe(2)
+    it("should sum up both rolls", () => {
+      game.roll(3);
+      game.roll(3);
+      game.roll(4);
+      game.roll(1);
+      expect(game.score()).toBe(11);
     });
   });
 
-  describe('after strike',  () => {
-    beforeEach( () => {
+  describe("after strike", () => {
+    beforeEach(() => {
       game.roll(10);
     });
-    it('next roll should score double',  () => {
+    it("next roll should score double", () => {
       game.roll(1);
 
-      expect(game.score()).toBe(12)
+      expect(game.score()).toBe(12);
     });
   });
-
 });
